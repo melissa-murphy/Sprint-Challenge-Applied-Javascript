8592deee567e8ce3dfc55e6bfff923c63ce0537d
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-12 10:58:53 -0700

    Tabs working!

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -25,11 +25,14 @@ class TabLink {
 
     // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class.
     // this.cards = Array.from(this.cards).map();
-    this.cards = Array.from(this.cards);
+    this.cards = Array.from(this.cards).map(card => {
+      this.TabCard = new TabCard(card);
+      return this.TabCard;
+    })
 
-    this.cards.forEach((cardElement) => {
-      new TabCard(tabElement);
-    });
+    // this.cards.forEach((cardElement) => {
+    //   new TabCard(tabElement);
+    // });
 
     // Add a click event that invokes this.selectTab
     // this.tabElement.addEventListener();
