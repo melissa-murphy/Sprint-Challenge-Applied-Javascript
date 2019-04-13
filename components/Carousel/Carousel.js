49be4b356890df6b886ddcb4dd507f49a133b92a
Melissa Murphy <melissa@thoughtfulmonkey.design>
2019-04-12 22:10:08 -0700

    still not there

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,9 +2,9 @@
 class Carousel {
   constructor(carousel) {
     this.carousel = carousel;
-    this.leftArrow = this.querySelector('left-button');
-    this.rightArrow = this.querySelector('right-button');
-    this.image = this.querySelectorAll('image');
+    this.leftArrow = this.document.querySelector('.left-button');
+    this.rightArrow = this.document.querySelector('.right-button');
+    this.image = this.document.querySelectorAll('img');
     this.imageArray = Array.from(this.image);
     this.imageArrayReversed = this.imageArray.reverse();
 
@@ -15,17 +15,17 @@ class Carousel {
   }
   imgLeft() {
     this.imageArrayReversed.forEach((img) => {
-      img.style.display = 'block';
+      return img.style.display = 'block';
     });
   }
   imgRight() {
-    this.imageArray.forEach((img) => {
+    return this.imageArray.forEach((img) => {
       img.style.display = 'block';
     });
   }
 }
 
-const carousel = document.querySelector('.carousel');
+let carousel = document.querySelector('.carousel');
 carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some
@@ -33,8 +33,8 @@ carousel = new Carousel(carousel);
     // 1. You will need to grab a reference to the carousel, and in it grab the
     //    left and right buttons
     // 2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
+    // 3. Create a current index
+    // 4. Those buttons are gonna need some click handlers.
     5. Think of how you would animate this compoennt. Make the yarncards slide
        in and out, or fade. It's up to you!
     6. Have fun!
