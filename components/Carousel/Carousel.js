5bb3d6bbc0504e4b21c7e3c99601806ead9ba3c3
Melissa Murphy <melissa@thoughtfulmonkey.design>
2019-04-12 20:34:26 -0700

    more carousel progress

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -5,6 +5,13 @@ class Carousel {
     this.leftArrow = this.querySelector('left-button');
     this.rightArrow = this.querySelector('right-button');
     this.image = this.querySelectorAll('image');
+    this.imageArray = Array.from(this.image);
+    this.imageArrayReversed = this.imageArray.reverse();
+
+    this.leftArrow.addEventListener('click', () =>
+      this.imgLeft());
+    this.rightArrow.addEventListener('click', () =>
+      this.imgRight());
   }
 }
 
@@ -15,7 +22,7 @@ carousel = new Carousel(carousel);
     hints:
     // 1. You will need to grab a reference to the carousel, and in it grab the
     //    left and right buttons
-    2. You will need to grab a reference to all of the images
+    // 2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
     5. Think of how you would animate this compoennt. Make the yarncards slide
